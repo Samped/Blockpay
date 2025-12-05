361fb3a21eb2c45a0466f7373fcbc20979838bfe
samped <michaelsamuelpedro@gmail.com>
2025-12-05 19:40:11 +0100

    adding instant payment

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -85,7 +85,7 @@ export function HeroSection() {
                   <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                 </svg>
               </div>
-              <span className="font-medium">Low Platform Fees</span>
+              <span className="font-medium">Instant Payment</span>
             </div>
           </div>
           
